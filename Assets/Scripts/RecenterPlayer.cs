2c732cfce287e75014e3c45089f847617a81748b
dmtrm <dmitrym@gmx.com>
2020-03-08 12:31:45 +0200

    Remove some comments

--- Assets/Scripts/RecenterPlayer.cs
+++ Assets/Scripts/RecenterPlayer.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class RecenterPlayer : MonoBehaviour
 {
-    // public GameObject player;
     public OVRInput.Controller controller = OVRInput.Controller.Active;
 
-
-    // Update is called once per frame
     void Update()
     {
         if (OVRInput.GetDown(OVRInput.Button.One, controller))
